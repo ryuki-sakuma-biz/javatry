65d595098867b2148d128767c1958bd28c22e1a5
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-22 13:46:00 +0900

    Step3のtest_datatype_basicType()の回答

--- src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
@@ -54,7 +54,7 @@ public class Step03DataTypeTest extends PlainTestCase {
             BigDecimal addedDecimal = amba.add(new BigDecimal(land));
             sea = String.valueOf(addedDecimal);
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 18.4
     }
 
     // ===================================================================================
