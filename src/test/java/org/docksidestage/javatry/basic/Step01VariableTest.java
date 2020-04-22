5bcddf6bc85406f292778aa77e10496a10199ffc
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-22 11:09:40 +0900

    Sample1のtest_variable_method_argument_mutable_methodcall()の解答

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -147,7 +147,7 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor416
     }
 
     private void helpMethodArgumentMethodcall(StringBuilder sea, int land) {
