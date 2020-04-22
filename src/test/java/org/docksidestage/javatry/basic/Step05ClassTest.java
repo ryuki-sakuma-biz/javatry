ac6a9c6d6afb6bd8f23a36fba40d1c747fe842ba
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-22 14:37:49 +0900

    Step5のtest_class_howToUse_wrongQuantity()の解答

--- src/test/java/org/docksidestage/javatry/basic/Step05ClassTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step05ClassTest.java
@@ -60,7 +60,7 @@ public class Step05ClassTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_class_howToUse_wrongQuantity() {
         Integer sea = doTest_class_ticket_wrongQuantity();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 9
     }
 
     private Integer doTest_class_ticket_wrongQuantity() {
