e7ea30de4105046a9137cf7cece927f4a42fc42a
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-22 11:54:24 +0900

    Step2のtest_if_elseif_nested()の解答

--- src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
@@ -91,7 +91,7 @@ public class Step02IfForTest extends PlainTestCase {
         if (land) {
             sea = 10;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 10
     }
 
     // ===================================================================================
