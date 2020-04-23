098019b31d2004b899c7638d6c727628f1b3dcd7
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-23 13:46:08 +0900

    Step6のtest_objectOriented_polymorphism_interface_hierarchy()の解答

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -298,9 +298,9 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
     public void test_objectOriented_polymorphism_interface_hierarchy() {
         Loudable loudable = new AlarmClock();
         String sea = loudable.soundLoudly();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => jiri jiri jiri---
         boolean land = loudable instanceof Animal;
-        log(land); // your answer? => 
+        log(land); // your answer? => false
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
