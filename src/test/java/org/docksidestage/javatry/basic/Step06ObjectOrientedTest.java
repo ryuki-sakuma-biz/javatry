a3fd5f2d0071b7f083dce50d5c693508adaaf9b5
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-23 13:37:32 +0900

    Step6のtest_objectOriented_polymorphism_5rd_overrideWithSuper()の解答

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -267,9 +267,9 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
         Animal animal = new Cat();
         BarkedSound sound = animal.bark();
         String sea = sound.getBarkWord();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => nya-
         int land = animal.getHitPoint();
-        log(land); // your answer? => 
+        log(land); // your answer? => 5
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
