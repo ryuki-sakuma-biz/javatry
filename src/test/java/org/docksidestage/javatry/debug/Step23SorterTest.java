4e48b63ea153719c526f7832434ce037e997560f
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-24 15:44:42 +0900

    Debug Step23 test_wordSorter()

--- src/test/java/org/docksidestage/javatry/debug/Step23SorterTest.java
+++ src/test/java/org/docksidestage/javatry/debug/Step23SorterTest.java
@@ -34,10 +34,20 @@ public class Step23SorterTest extends PlainTestCase {
     public void test_wordSorter() {
         // arrange
         Language language = new LanguagePool().getLanguage("English");
-        List<Word> input = Arrays.asList(new Word(language, "Candy"), new Word(language, "Table"), new Word(language, "Bee"),
-                new Word(language, "Zebra"), new Word(language, "Apple"));
-        List<Word> expected = Arrays.asList(new Word(language, "Apple"), new Word(language, "Bee"), new Word(language, "Candy"),
-                new Word(language, "Table"), new Word(language, "Zebra"));
+        List<Word> input = Arrays.asList(
+                new Word(language, "Candy"),
+                new Word(language, "Table"),
+                new Word(language, "Bee"),
+                new Word(language, "Zebra"),
+                new Word(language, "Apple")
+        );
+        List<Word> expected = Arrays.asList(
+                new Word(language, "Apple"),
+                new Word(language, "Bee"),
+                new Word(language, "Candy"),
+                new Word(language, "Table"),
+                new Word(language, "Zebra")
+        );
 
         // act
         List<Word> result = new WordSorter().sort(input);
