d7c56a81b8fe7f23534ec0e84fb56a2faf380c0a
佐久間 龍樹 <ryuki.sakuma@bizreach.co.jp>
2020-04-24 15:15:55 +0900

    Debug Step21 test_getLanguages()

--- src/main/java/org/docksidestage/bizfw/debug/Word.java
+++ src/main/java/org/docksidestage/bizfw/debug/Word.java
@@ -21,7 +21,6 @@ public class Word {
     }
 
     public boolean hasLanguage() {
-        getLanguage().countries = 3;
         if (getLanguage() == null) {
             return false;
         } else {
